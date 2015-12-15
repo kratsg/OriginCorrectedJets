7df4adea8f6e6d04ed24abfd8b82cf672c267cb1
Giordon Stark <kratsg@gmail.com>
2015-12-15 09:05:24 -0800

    Keep building up structure. We want to make a special tool that modifies the clusters before we cluster the jets.

--- OriginCorrectedJets/OriginCorrection.h
+++ OriginCorrectedJets/OriginCorrection.h
@@ -1,32 +1,10 @@
-/********************************************************
- * JetCalibrator:
- *
- * This class handles jet calibration and systematics
- *
- * Jeff Dandoy (jeff.dandoy@cern.ch)
- *
- ********************************************************/
-
-#ifndef xAODAnaHelpers_JetCalibrator_H
-#define xAODAnaHelpers_JetCalibrator_H
-
-// CP interface includes
-#include "PATInterfaces/SystematicRegistry.h"
-#include "PATInterfaces/SystematicSet.h"
-#include "PATInterfaces/SystematicVariation.h"
-
-// external tools include(s):
-#include "JetCalibTools/JetCalibrationTool.h"
-#include "JetUncertainties/JetUncertaintiesTool.h"
-#include "JetResolution/JERTool.h"
-#include "JetResolution/JERSmearingTool.h"
-#include "JetSelectorTools/JetCleaningTool.h"
-#include "JetMomentTools/JetVertexTaggerTool.h"
+#ifndef OriginCorrectedJets_OriginCorrection_H
+#define OriginCorrectedJets_OriginCorrection_H
 
 // algorithm wrapper
 #include "xAODAnaHelpers/Algorithm.h"
 
-class JetCalibrator : public xAH::Algorithm
+class OriginCorrection : public xAH::Algorithm
 {
   // put your configuration variables here as public variables.
   // that way they can be set directly from CINT and python.
@@ -35,74 +13,8 @@ public:
   std::string m_inContainerName;
   std::string m_outContainerName;
 
-  std::string m_jetAlgo;
-  std::string m_outputAlgo;
-  std::string m_calibConfigData;
-  std::string m_calibConfigFullSim;
-  std::string m_calibConfigAFII;
-  std::string m_calibConfig;
-  std::string m_calibSequence;
-  std::string m_JESUncertConfig;
-  std::string m_JESUncertMCType;
-  bool m_setAFII;
-
-  std::string m_JERUncertConfig;
-  bool m_JERFullSys;
-  bool m_JERApplyNominal;
-
-  std::string m_jetCleanCutLevel;
-  bool m_saveAllCleanDecisions;
-  bool m_jetCleanUgly;
-  bool m_redoJVT;
-  // sort after calibration
-  bool    m_sort;
-  //Apply jet cleaning to parent jet
-  bool    m_cleanParent;
-  bool    m_applyFatJetPreSel;
-
-  // systematics
-  bool m_runSysts;
-
-private:
-  int m_numEvent;         //!
-  int m_numObject;        //!
-
-  bool m_isMC;            //!
-  bool m_isFullSim;       //!
-
-  std::string m_outSCContainerName;     //!
-  std::string m_outSCAuxContainerName;  //!
-
-  std::vector<CP::SystematicSet> m_systList; //!
-  std::vector<int> m_systType; //!
-
-  // tools
-  JetCalibrationTool       * m_jetCalibration; //!
-
-  JetUncertaintiesTool     * m_JESUncertTool;      //!
-
-  JERTool                  * m_JERTool;        //!
-  JERSmearingTool          * m_JERSmearTool;   //!
-  ToolHandle<IJERTool>       m_JERToolHandle;  //!
-
-  JetVertexTaggerTool      * m_JVTTool;        //!
-  ToolHandle<IJetUpdateJvt>  m_JVTToolHandle;  //!
-
-  JetCleaningTool          * m_jetCleaning;    //!
-  std::vector<std::string>  m_decisionNames;    //!
-  std::vector< JetCleaningTool* > m_allJetCleaningTools;   //!
-
-
-  // variables that don't get filled at submission time should be
-  // protected from being send from the submission node to the worker
-  // node (done by the //!)
-public:
-  // Tree *myTree; //!
-  // TH1 *myHist; //!
-
-
   // this is a standard constructor
-  JetCalibrator (std::string className = "JetCalibrator");
+  OriginCorrection (std::string className = "OriginCorrection");
 
   // these are the functions inherited from Algorithm
   virtual EL::StatusCode setupJob (EL::Job& job);
@@ -121,7 +33,7 @@ public:
 
   /// @cond
   // this is needed to distribute the algorithm to the workers
-  ClassDef(JetCalibrator, 1);
+  ClassDef(OriginCorrection, 1);
   /// @endcond
 
 };
