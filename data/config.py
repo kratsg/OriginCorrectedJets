a65dd4626cf8210d1093444fed0fe1dbc62dbf77
Giordon Stark <kratsg@gmail.com>
2015-12-17 00:41:46 -0600

    add lots of comparisons

--- data/config.py
+++ data/config.py
@@ -7,13 +7,17 @@ truthJets    = "AntiKt10TruthTrimmedPtFrac5SmallR20Jets"
 normalJets   = "AntiKt10LCTopoTrimmedPtFrac5SmallR20Jets"
 pflowJets    = "AntiKt10EMCPFlowTrimmedPtFrac5SmallR20Jets"
 ''' These are output containers we make, names based on inputs used '''
+uncorrectedClusters = "Uncorrected{0:s}".format(caloClusters)
 originCorrectedClusters = "OriginCorrection{0:s}".format(caloClusters)
 originCorrectedJets     = "OriginCorrection{0:s}".format(normalJets)
 
 ''' Set up all the algorithms '''
 c.setalg("OriginCorrection", {"m_inContainerName": caloClusters,
-                              "m_outContainerName": originCorrectedClusters,
-                              "m_debug": True})
+                              "m_outContainerName": originCorrectedClusters})
+
+c.setalg("OriginCorrection", {"m_inContainerName": caloClusters,
+                              "m_outContainerName": uncorrectedClusters,
+                              "m_doCorrection": False})
 
 c.setalg("JetReclusteringAlgo", {"m_debug": False,
                                  "m_inputJetContainer": originCorrectedClusters,
@@ -27,16 +31,23 @@ c.setalg("JetReclusteringAlgo", {"m_debug": False,
                                  "m_name": "OriginCorrectedJets"
                                 })
 
-for container in [truthJets, normalJets, pflowJets, originCorrectedJets]:
+for container in [truthJets, normalJets, pflowJets, originCorrectedJets, uncorrectedClusters, originCorrectedClusters]:
   c.setalg("JetHistsAlgo", {"m_debug": False,
                             "m_inContainerName": container,
                             "m_detailStr": "kinematic substructure 4LeadingJets",
                             "m_name": container
                           })
 
-c.setalg("JetComparisonHistsAlgo", {"m_debug": False,
-                                    "m_inContainer1Name": normalJets,
-                                    "m_inContainer2Name": originCorrectedJets,
-                                    "m_detailStr": "kinematic substructure 4LeadingJets",
-                                    "m_name": "{0:s}versus{1:s}".format(normalJets, originCorrectedJets)
-                                   })
+for left,right in [(uncorrectedClusters, originCorrectedClusters),
+                   (normalJets, originCorrectedJets),
+                   (normalJets, truthJets),
+                   (normalJets, pflowJets),
+                   (originCorrectedJets, truthJets),
+                   (pflowJets, truthJets),
+                   (pflowJets, originCorrectedJets)]:
+  c.setalg("JetComparisonHistsAlgo", {"m_debug": False,
+                                      "m_inContainer1Name": left,
+                                      "m_inContainer2Name": right,
+                                      "m_detailStr": "kinematic substructure 4LeadingJets",
+                                      "m_name": "{0:s}versus{1:s}".format(left, right)
+                                     })
